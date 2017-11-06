aebbf0aab08dcc6bf73494cd225688d710cb92b9
Benoit Fontaine <benoit.a.fontaine@axa.fr>
2017-11-06 16:59:39 +0100

    fix: update polyfills fort React16 (requestAnimationFrame)

--- packages/react-scripts/config/polyfills.js
+++ packages/react-scripts/config/polyfills.js
@@ -19,6 +19,9 @@ if (typeof Promise === 'undefined') {
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
 
+// React polyfills
+require('raf/polyfill');
+
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
